0eef5dd7689f2d32a9a858b7fe5e80a163b07106
MikleAvat <urkunovmihail@gmail.com>
2025-12-31 15:02:10 +0300

    Updates

---
+++ _nuxt/Ba0AuAgw.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./66C-qVYL.js";const t={};function r(e,n){return" Страница контакты. Нужно доделать "}const o=c(t,[["render",r]]);export{o as default};
---
+++ _nuxt/S37nUT33.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./66C-qVYL.js";const r={};function t(_,c){return" Произвольное значение "}const f=e(r,[["render",t]]);export{f as default};
