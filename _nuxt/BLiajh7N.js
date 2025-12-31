dcc3a5ff15c00f3aced0bfc9a768a7915d902ec2
MikleAvat <urkunovmihail@gmail.com>
2025-12-31 20:42:36 +0300

    Updates

--- _nuxt/BLiajh7N.js
+++
@@ -1,1 +0,0 @@
-import{_ as c}from"./DdJx6rEb.js";const t={};function r(e,n){return" Страница контакты. Нужно доделать "}const o=c(t,[["render",r]]);export{o as default};
---
+++ _nuxt/CEvFguUP.js
@@ -0,0 +1,1 @@
+import{_ as c}from"./DG37OJVf.js";const t={};function r(e,n){return" Страница контакты. Нужно доделать "}const o=c(t,[["render",r]]);export{o as default};
